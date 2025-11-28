f28587f96bd88bd209ccf4369964c2f12f6c07fd
Sedra Ramarosaona <sedranante@gmail.com>
2025-11-28 08:51:35 +0100

    Defin eVariables

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string.
+
+    Args:
+        n (float): number to convert
+
+    Return:
+        str: The string representation of the float n
+    """
+    return str(n)
