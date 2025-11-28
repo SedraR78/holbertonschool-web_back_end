56e3ad7a0f2dc8c71e2cf1c436fdfa1dec707593
Sedra Ramarosaona <sedranante@gmail.com>
2025-11-28 08:47:03 +0100

    function that return the floor of the float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float as an integer.
+
+    Args:
+        n (float): The float number to be floored
+
+    Return:
+        int: The floor of the float n
+    """
+    return int(n)
