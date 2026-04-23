0e7fdc452819009011c7d9693c39ba9f35b3b7e7
Sedra Ramarosaona <sedranante@gmail.com>
2026-04-23 19:15:43 +0200

    create a function helper for documentation

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Helper pour la pagination."""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retourne (start, end) pour la pagination."""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
