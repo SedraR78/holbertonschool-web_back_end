106c6c3254a2d46242665cbf815bfea18c94c5e7
Sedra Ramarosaona <sedranante@gmail.com>
2026-04-24 16:00:08 +0200

    task 0 fix

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
-"""Helper pour la pagination."""
+"""
+Simple helper function
+"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retourne (start, end) pour la pagination."""
+    """
+    Retourne (start, end) pour la pagination.
+    """
     start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
