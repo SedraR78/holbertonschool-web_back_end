55be6218652c6f1c75a72b24fa2bd6604d6368e5
Sedra Ramarosaona <sedranante@gmail.com>
2026-04-28 12:24:00 +0200

    concatArays Funciton with ES6 JS

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,6 +1,3 @@
 export default function concatArrays(array1, array2, string) {
-
-    let array3 = array1.concat(array2).concat(string)
-    return array3 
-
+    return array1.concat(array2).concat(string)
 }
