f5d80c25cda602849c41fc7a3a9adb1637a54cd3
Sedra Ramarosaona <sedranante@gmail.com>
2026-05-11 14:07:16 +0200

    1st Task : Basic Javascript with Node JS

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
 console.log('Welcome to Holberton School, what is your name?')
 
-process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is : ${name}`);
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
-    console.log("This important software is now closing");
+  process.stdout.write('This important software is now closing\n');
 });
